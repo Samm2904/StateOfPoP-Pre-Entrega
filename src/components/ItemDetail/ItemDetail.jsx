bde9b48dbd4f6e808fc63c5486795ef8f4d037c9
Sofia <sofiamartinez2904@gmail.com>
2026-06-22 18:32:22 -0300

    Cree el contexto del carrito

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,9 +1,13 @@
 import { Item } from "../Item/Item";
+import { useCart } from "../../context/cartContext";
 
 export const ItemDetail = ({item}) => {
+    const {addItem} = useCart();
+
+
     return (
         <Item {...item}>
-            <button className="btn primary">Agregar al carrito </button>
+            <button className="btn primary" onClick={() => addItem(item)}>Agregar al carrito </button>
         </Item>
     );
 };
---
+++ src/context/cartContext.jsx
@@ -0,0 +1,62 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const CartContext = createContext();
+
+export const useCart=() => {
+    const context = useContext(CartContext);
+
+    if (!context){
+        throw new Error("useCart debe ser usado dentro de un CartProvider");
+    }
+    return context;
+};
+
+export const CartProvider = ({children}) => {
+    const navigate = useNavigate();
+    const [cart, setCart] = useState([]);
+
+    const isInCart = (item) => {
+        const inCart = cart.some((element) => element.id === item.id)
+        return inCart;
+    };
+
+    const addItem = (item) => {
+        if(isInCart(item)){
+            alert("El producto ya se encuentra en el carrito");
+            return;
+        }
+        setCart([...cart, item]);
+        alert("Producto agregado al carrito.");
+    };
+
+    const removeItem = (id) => {
+        const updatedCart = cart.filter(element => element.id !== id);
+        setCart(updatedCart);
+        alert("Producto eliminado del carrito.");
+    };
+
+    const clearCart = () => {
+        setCart([]);
+    };
+
+    const getTotalItems = () => {
+        return cart.length;
+    };
+
+    const getCartTotal = () => {
+        return cart.reduce((total, element) => total + element.price * element.quantity, 0);
+    };
+
+    const checkout = () => {
+        alert("Compra realziada con exito.");
+        clearCart();
+        navigate("/");
+    };
+
+   const values = {cart,addItem, removeItem, clearCart, getTotalItems, getCartTotal, checkout};
+    return <CartContext.Provider value={values}>
+        {children}
+    </CartContext.Provider>
+
+};
