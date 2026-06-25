9eea558425d75f19f341bfa4dca8783d28e1d48d
Sofia <sofiamartinez2904@gmail.com>
2026-06-25 16:03:59 -0300

    Se creo el admin y las rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css'
-import { Header } from "./components/Header/Header";
-import { Footer } from "./components/Footer/Footer";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import { CartView } from "./components/Cart/CartView";
 import { ProductFormContainer } from "./components/adminComponents/productFormContainer";
 import { ProductSuccess } from "./components/adminComponents/ProductSuccess";
+import { PublicLayout } from "./layouts/PublicLayouts";
+import { AdminLayout } from "./layouts/AdminLayout";
 
 function App() {
   return (
     <>
-    <Header />
-    <main>
+
       <Routes>
-        <Route path="/" element={<ItemListContainer />}/>
-        <Route path="/product/:id" element={<ItemDetailContainer />}/>
-        <Route path="/carrito" element={<CartView/>}/>
-        <Route path="/admin" element={<ProductFormContainer/>}/>
-        <Route path="/success/:id" element={<ProductSuccess/>}/>
+        <Route element={<PublicLayout/>}>
+          <Route path="/" element={<ItemListContainer />}/>
+          <Route path="/product/:id" element={<ItemDetailContainer />}/>
+          <Route path="/carrito" element={<CartView/>}/>
+        </Route>
+
+        <Route path="/admin/login" element={<Login/>}/>
+
+        <Route path="/admin" 
+          element={
+            <ProtectedRoute>
+              <AdminLayout/>
+            </ProtectedRoute>
+          }
+          >
+          <Route index element={<Navigate to={"dashboard"}/>}/>
+          <Route path="dashboard" element={<Dashboard/>}/>
+          <Route path="/products/new" element={<ProductFormContainer/>}/>
+          <Route path="products/success/:id" element={<ProductSuccess/>}/>
+        </Route>
       </Routes>
-    </main>
-    <Footer />
+
     </>
   );
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -19,5 +19,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };
